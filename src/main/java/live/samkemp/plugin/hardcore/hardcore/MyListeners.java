29c54ddd41fcd20e201a657e138a22bc73635915
untamemadman <sam@untamemediagroup.com>
2019-08-04 09:56:01 +0100

    Attempt 2, no time out?

--- src/main/java/live/samkemp/plugin/hardcore/hardcore/MyListeners.java
+++ src/main/java/live/samkemp/plugin/hardcore/hardcore/MyListeners.java
@@ -3,30 +3,31 @@ package live.samkemp.plugin.hardcore.hardcore;
 import org.bukkit.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 
 public class MyListeners implements Listener
 {
 
     @EventHandler
-    public void onPlayerDeath(PlayerDeathEvent event)
-    {
-        Hardcore.NoDeaths++;
-        World NewWorld = Bukkit.getServer().createWorld(new WorldCreator("World" + Hardcore.NoDeaths));
-        NewWorld.setDifficulty(Difficulty.HARD);
-
-        if(!event.getEntity().getWorld().getName().equalsIgnoreCase("world"))
-            Bukkit.getServer().unloadWorld(event.getEntity().getWorld(), true);
-    }
-
-    @EventHandler
     public void onPlayerRespawn(PlayerRespawnEvent event)
     {
-        Location RespawnLocation = Bukkit.getServer().getWorld("World" + Hardcore.NoDeaths).getSpawnLocation();
-        event.setRespawnLocation(RespawnLocation);
+        Hardcore.NoDeaths++;
+
+        Location RespawnLocation = Bukkit.getServer().getWorld("world").getSpawnLocation();
 
         event.getPlayer().getInventory().clear();
+
+        event.setRespawnLocation(RespawnLocation);
+
+        World NewWorld = new WorldCreator("World" + Hardcore.NoDeaths).generateStructures(true).type(WorldType.NORMAL).createWorld();
+        NewWorld.setDifficulty(Difficulty.HARD);
+        NewWorld.setKeepSpawnInMemory(false);
+
+        if(!event.getPlayer().getWorld().getName().equalsIgnoreCase("world"))
+            Bukkit.getServer().unloadWorld(event.getPlayer().getWorld(), true);
+
+        RespawnLocation = NewWorld.getSpawnLocation();
+
+        event.getPlayer().teleport(RespawnLocation);
     }
 }
