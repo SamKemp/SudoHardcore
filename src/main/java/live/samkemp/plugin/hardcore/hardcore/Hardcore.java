def84eede86d32c0c26820e1cf9fd8fff1091347
untamemadman <sam@untamemediagroup.com>
2019-08-04 12:47:53 +0100

    Shutdown server after death

--- src/main/java/live/samkemp/plugin/hardcore/hardcore/Hardcore.java
+++ src/main/java/live/samkemp/plugin/hardcore/hardcore/Hardcore.java
@@ -7,19 +7,9 @@ import java.util.Random;
 public final class Hardcore extends JavaPlugin
 {
 
-    static Random r = new Random();
-    public static int RunKey = r.nextInt(9999) + 10000;
-    public static int NoDeaths = 0;
-
-
     @Override
     public void onEnable()
     {
         getServer().getPluginManager().registerEvents(new MyListeners(), this);
-    }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
     }
 }
--- src/main/java/live/samkemp/plugin/hardcore/hardcore/MyListeners.java
+++ src/main/java/live/samkemp/plugin/hardcore/hardcore/MyListeners.java
@@ -1,62 +1,17 @@
 package live.samkemp.plugin.hardcore.hardcore;
 
-import org.bukkit.*;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerPortalEvent;
-import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 
 public class MyListeners implements Listener
 {
 
     @EventHandler
-    public void onPlayerRespawn(PlayerRespawnEvent event)
+    public void onPlayerDeath(PlayerDeathEvent event)
     {
-        Hardcore.NoDeaths++;
-
-        String WorldName = Hardcore.RunKey + "_World-" + event.getPlayer().getDisplayName() + "-" + Hardcore.NoDeaths;
-
-        event.getPlayer().getInventory().clear();
-
-        World NewWorld = new WorldCreator(WorldName).generateStructures(true).type(WorldType.NORMAL).environment(World.Environment.NORMAL).createWorld();
-        NewWorld.setDifficulty(Difficulty.HARD);
-        NewWorld.setKeepSpawnInMemory(false);
-
-        if(!event.getPlayer().getWorld().getName().equalsIgnoreCase("world"))
-            Bukkit.getServer().unloadWorld(event.getPlayer().getWorld(), true);
-
-        Location RespawnLocation = NewWorld.getSpawnLocation();
-
-        event.setRespawnLocation(RespawnLocation);
-
-        new WorldCreator(WorldName + "_nether").generateStructures(true).type(WorldType.NORMAL).environment(World.Environment.NETHER).createWorld();
-        new WorldCreator(WorldName + "_end").generateStructures(true).type(WorldType.NORMAL).environment(World.Environment.THE_END).createWorld();
+        Bukkit.shutdown();
     }
 
-    @EventHandler
-    public void playerEnterPortal(PlayerPortalEvent event)
-    {
-        String WorldName = Hardcore.RunKey + "_World-" + event.getPlayer().getDisplayName() + "-" + Hardcore.NoDeaths;
-        Location PortalTo;
-        String GoingTo = event.getTo().getWorld().getName();
-
-        if(GoingTo.equalsIgnoreCase("world_nether"))
-        {
-            PortalTo = Bukkit.getServer().getWorld(WorldName + "_nether").getSpawnLocation();
-        }
-        else if(GoingTo.equalsIgnoreCase("world_end"))
-        {
-            PortalTo = Bukkit.getServer().getWorld(WorldName + "_end").getSpawnLocation();
-        }
-        else if(GoingTo.equalsIgnoreCase("world"))
-        {
-            PortalTo = Bukkit.getServer().getWorld(WorldName).getSpawnLocation();
-        }
-        else
-        {
-            PortalTo = Bukkit.getServer().getWorld(WorldName).getSpawnLocation();
-        }
-
-        event.setTo(PortalTo);
-    }
 }
